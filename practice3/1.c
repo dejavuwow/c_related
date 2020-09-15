8400ceff43bcf9a820221f56363b2eeea5fa3c8b
dejavuwow <674641530@qq.com>
2020-09-15 21:24:53 +0800

    old changes

--- practice3/1.c
+++ practice3/1.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-	int pop = 100000000000000023;
+	int arr[][3] = {1,2,3,4,5};
+	int (*p)[3] = arr;
+	printf("%d\n", *(*(p+1)+3));
+	long long int pop = 100000000000000023;
 	float f1 = 3.4E39,f2 = 1.2323212327812;
 	printf("%lld,%e,%f\n",pop,f1,f2);
 	getchar();
