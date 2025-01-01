3eae6126c069192add41060a6440f56523419a77
dejavuwow <674641530@qq.com>
2025-01-01 22:32:10 +0800

    nfa simulation

--- algorithm/2024/forward.c
+++ algorithm/2024/forward.c
@@ -4,6 +4,12 @@
 
 #define EPSILON -1
 #define MAX_SIZE 1000
+int includes(int *set, int l, int value) {
+	for (int i = 0; i < l; i++) {
+		if (set[i] == value) return 1;
+	}
+	return 0;
+}
 typedef struct Tran {
 	int from;
 	int to;
@@ -19,7 +25,7 @@ typedef struct NFA {
 } NFA;
 
 void addTran(NFA *nfa, int from, int to, int symbol) {
-	Trans *tran = malloc(sizeof(Tran));
+	Tran *tran = (Tran*)malloc(sizeof(Tran));
 
 	tran->from = from;
 	tran->to = to;
@@ -40,83 +46,179 @@ NFA *getSingleNfa(int symbol) {
 	return nfa;
 }
 
-NFA *addKleene(NFA *nfa) {
-	addTran(nfa, nfa->accept, nfa->start, EPSILON);
-	NFA *nfa1 = getSingleNfa(EPSILON);
-	NFA *nfa2 = getSingleNfa(EPSILON);
-	nfa = concatNfa(concatNfa(nfa1, nfa), nfa2);
-	addTran(nfa, nfa->start, nfa->accept, EPSILON);
-
-	return  nfa;
-}
-
-NFA *addAlternative(NFA *nfa1, NFA *nfa2) {
-
-}
 
 //0->1->2   0->1->2
 NFA *concatNfa(NFA *nfa1, NFA *nfa2) {
 	int offset = nfa1->stateNum - 1;
 	nfa1->stateNum += offset;
-	nfa1->transNum += nfa2->transNum;
-
+	
 	for (int i = 0; i < nfa2->transNum; i++) {
 		int from = nfa2->start == nfa2->trans[i]->from ? nfa1->accept : nfa2->trans[i]->from + offset;
+
 		addTran(nfa1, from, nfa2->trans[i]->to + offset, nfa2->trans[i]->symbol);
 		free(nfa2->trans[i]);
 		nfa2->trans[i] = NULL;
 	}
 	nfa1->accept = nfa2->accept + offset - 1;
 	free(nfa2);
-
 	return nfa1;
 }
 
+NFA *addAlternative(NFA *nfa1, NFA *nfa2) {
 
-buildNFA(char *regular) {
+	NFA *nfa = concatNfa(getSingleNfa(EPSILON), nfa1);
+	int offset = nfa->stateNum;
+	nfa->stateNum += (nfa2->stateNum + 1);
+	addTran(nfa, nfa->start, nfa2->start + offset, EPSILON);
+	for (int i = 0; i < nfa2->transNum; i++) {
+		addTran(nfa, nfa2->trans[i]->from + offset, nfa2->trans[i]->to + offset, nfa2->trans[i]->symbol);
+		free(nfa2->trans[i]);
+		nfa2->trans[i] = NULL;
+	}
+	free(nfa2);
+	addTran(nfa, nfa->accept, nfa->stateNum - 1, EPSILON);
+	addTran(nfa, nfa->stateNum - 2, nfa->stateNum - 1, EPSILON);
+	nfa->accept = nfa->stateNum - 1;
+
+	return nfa;
+}
+
+NFA *addKleene(NFA *nfa) {
+	addTran(nfa, nfa->accept, nfa->start, EPSILON);
+	NFA *nfa1 = getSingleNfa(EPSILON);
+	NFA *nfa2 = getSingleNfa(EPSILON);
+	nfa = concatNfa(nfa1, nfa);
+	nfa = concatNfa(nfa, nfa2);
+	addTran(nfa, nfa->start, nfa->accept, EPSILON);
+
+	return  nfa;
+}
+
+NFA *buildNFA(char *regular) {
 	NFA	*stack[100];
+	NFA *n1, *n2, *n3, *n4;
 	int top = -1;
 	int hasAlternative = 0;
 	stack[++top] = getSingleNfa(EPSILON);
-	for (; *regular != '\0'; ) {
+	for (; *regular != '\0'; regular++) {
+		printf("%c ", *regular);
 		switch (*regular) {
 			case '(':
 				stack[++top] = getSingleNfa(EPSILON);
-				break
+				break;
 			case ')':
 				if (hasAlternative) {
-					NFA *nfa1 = stack[top--];
-					NFA *nfa2 = stack[top--];
-					NFA *nfa3 = top >= 0 ? stack[top--] : NULL;
-					NFA *nfa4 = addAlternative(nfa1, nfa2);
-					stack[++top] = nfa3 ? concatNfa(nfa3, nfa4) nfa4:
+					n1 = stack[top--];
+					n2 = stack[top--];
+					n3 = top >= 0 ? stack[top--] : NULL;
+					n4 = addAlternative(n1, n2);
+					stack[++top] = n3 ? concatNfa(n3, n4) : n4;
 				}
 				hasAlternative = 0;
 				break;
 			case '*':
-				NFA *nfa1 = stack[top--];
-				NFA *nfa2 = top >= 0 ? stack[top--] : NULL;
-				NFA *nfa3 = addKleene(nfa1);
-				stack[++top] = nfa2 ? concatNfa(nfa2, nfa3) : nfa3;
+				n1 = addKleene(stack[top--]);
+				n2 = top >= 0 ? stack[top--] : NULL;
+				stack[++top] = n2 ? concatNfa(n2, n1) : n1;
 				break;
 			case '|':
 				if (hasAlternative) {
-					NFA *nfa1 = stack[top--];
-					NFA *nfa2 = stack[top--];
-					NFA *nfa3 = top >= 0 ? stack[top--] : NULL;
-					NFA *nfa4 = addAlternative(nfa1, nfa2);
-					stack[++top] = nfa3 ? concatNfa(nfa3, nfa4) nfa4:
+					n1 = stack[top--];
+					n2 = stack[top--];
+					n3 = top >= 0 ? stack[top--] : NULL;
+					n4 = addAlternative(n1, n2);
+					stack[++top] = n3 ? concatNfa(n3, n4) : n4;
 				}
 				hasAlternative = 1;
 				stack[++top] = getSingleNfa(EPSILON);
 				break;
 			default:
-				NFA *nfa1 = stack[top--];
-				NFA *nfa2 = getSingleNfa(*regular);
-				stack[++top] = concatNfa(nfa1, nfa2);
+				n1 = stack[top--];
+				n2 = getSingleNfa(*regular);
+				if (*(regular + 1) == '*') {
+					n2 = addKleene(n2);
+					regular++;
+				}
+				stack[++top] = concatNfa(n1, n2);
 				break;
+		}
+	}
+	return stack[top];
+}
+int *getEpsilonClosure(NFA *nfa, int state, int *set, int *length) {
+	int l = 0;
+	set[l++] = state;
+	int hasNew = 1;
+	while (hasNew) {
+		hasNew = 0;
+		for (int i = 0; i < nfa->transNum; i++) {
+			if (includes(set, l, nfa->trans[i]->from) && nfa->trans[i]->symbol == EPSILON && !includes(set, l, nfa->trans[i]->to)) {
+				set[l++] = nfa->trans[i]->to;
+				hasNew = 1;
+			}
+		}
+	}
+	*length = l;
+	return set;
+}
+
+int *move(NFA *nfa, int state, int c, int *res, int *resLength) {
+	for (int j = 0; j < nfa->transNum; j++) {
+
+		int subRes[nfa->stateNum];
+		int l = 0;
+		if (state == nfa->trans[j]->from && c == nfa->trans[j]->symbol) {
+			getEpsilonClosure(nfa, nfa->trans[j]->to, subRes, &l);
+			for (int k = 0; k < l; k++) {
+				if (!includes(res, *resLength, subRes[k])) {
+					res[(*resLength)++] = subRes[k];
+				}
+			}
 		}
 	}
 }
 
+int *moveSet(NFA *nfa, int *set, int l, int c, int *res, int *resLength) {
+	for (int i = 0; i < l; i++) {
+		int subRes[nfa->stateNum];
+		int n = 0;
+		move(nfa, set[i], c, subRes, &n);
+		printf("****%d\n", n);
+		for (int j = 0; j < n; j++) {
+			if (!includes(res, *resLength, subRes[j])) {
+				res[(*resLength)++] = subRes[j];
+			}
+		}
+	}
+	return res;
+}
 
+int match(NFA *nfa, char *s) {
+	int vol = nfa->stateNum;
+	printf("^^^^^^^^^^%d\n", vol);
+	int l = 0;
+	int set[vol];
+	int res[vol];
+	getEpsilonClosure(nfa, nfa->start, set, &l);
+	int resLength;
+	for (; *s != '\0'; s++) {
+		printf("%c \n", *s);
+		resLength = 0;
+		 moveSet(nfa, set, l, *s, res, &resLength);
+		 printf("%d\n", resLength);
+		 l = 0;
+		 for (int j = 0; j < resLength; j++) {
+			 set[l++] = res[j];
+		 }
+	}
+	printf("DDDDD");
+	while (resLength) {
+		if (set[--resLength] == nfa->accept) return 1;
+	}
+	return 0;
+}
+int main(void) {
+	NFA *nfa = buildNFA("ab*(abc|ab)aa*");
+	printf("%d***", match(nfa, "aabcaaaaa"));
+	return 0;
+}
